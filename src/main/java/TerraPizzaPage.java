9c01b03938070f07a9f5f28048f57de567f22fed
NastassiyaKh <nholodok@yandex.ru>
2023-03-01 20:58:01 +0300

    add TerraPizzaPage class

--- src/main/java/TerraPizzaPage.java
+++ src/main/java/TerraPizzaPage.java
@@ -2,7 +2,8 @@ public class TerraPizzaPage {
     public static final String URL = "https://terrapizza.by";
     public static final String BUTTON_PIZZA = "//ul[@class='menu-navigation__main active']//a[@href='/catalog/picca']";
     public static final String BUTTON_PIZZA_MARGARITA_IN_CART = "//*[@id='menu-card-364']/div[3]/div[3]/div[2]/div[2]/button";
-    public static final String BUTTON_BASKET = "html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]/div/div";
+    public static final String BUTTON_BASKET = "//div[@class='basket__btn-top basket__top basket__btn-top--sm']";
+    public static final String LABEL_PIZZA_MARGARITA_IN_CART_ALL = "//*[@id='basket-el-0']/div[2]/div[1]/div[1]/span/span[2]";
     public static final String LABEL_PIZZA_MARGARITA_IN_CART = "//div[@class='basket__products-item-name']";
     public static final String BUTTON_CLOSE_COOKIES = "//button[@class='close-icon']";
 }
